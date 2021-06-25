02bb778d4d24892618d8e8f1969c3fd5ad621b07
Jack Parsons <jackpar1113@gmail.com>
2021-06-25 17:14:57 -0600

    Implement main menu UI and basic functionality

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("QUITTING GAME");
+            Application.Quit();
+        }
+    }
+}
