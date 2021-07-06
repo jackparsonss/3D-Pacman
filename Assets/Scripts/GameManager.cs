a60046dd0f231c2f896d55e6aaa73c03fa3c752b
Jack <jackpar1113@gmail.com>
2021-07-06 15:13:17 -0600

    Fix bug where ghost turned wrong color after powerup is over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,12 +9,17 @@ public class GameManager : MonoBehaviour
     
     [SerializeField] private TextMeshProUGUI coinsText;
     [SerializeField] private float coins;
+    [SerializeField] private Material _greenGhostMaterial;
+    [SerializeField] private Material _blueGhostMaterial;
 
     private GameObject[] _ghosts;
-
+    
     private void Start()
     {
         _ghosts = GameObject.FindGameObjectsWithTag("Ghost");
+
+        // _greenGhostMaterial = Resources.Load("Materials/GreenGhost", typeof(Material)) as Material;
+        // _blueGhostMaterial = Resources.Load("Materials/BlueGhost", typeof(Material)) as Material;
     }
 
     public void StartPowerUp()
@@ -29,19 +34,19 @@ public class GameManager : MonoBehaviour
     private IEnumerator PowerUpActive()
     {
         IsPowerUpActive = true;
-        ChangeGhostsColor(Color.blue);
+        ChangeGhostsColor(_blueGhostMaterial);
 
         yield return new WaitForSeconds(5);
         
         IsPowerUpActive = false;
-        ChangeGhostsColor(Color.red);
+        ChangeGhostsColor(_greenGhostMaterial);
     }
 
-    private void ChangeGhostsColor(Color color)
+    private void ChangeGhostsColor(Material color)
     {
         foreach (GameObject ghost in _ghosts)
         {
-            ghost.GetComponent<Renderer>().material.color = color;
+            ghost.GetComponent<Renderer>().material = color;
         }
     }
 
